c21f1e334d0b512bd6966fc49d45eb02d73fda44
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-05-30 00:34:52 +0900

    setting: pageRoutes 수정 (#7)

--- src/routes/pageRoutes.tsx
+++ src/routes/pageRoutes.tsx
@@ -4,13 +4,8 @@ import Solve from '@pages/solve/Solve';
 import routePath from '@/routes/routePath';
 
 const pageRoutes = [
-  {
-    path: '/',
-    children: [
-      { path: routePath.HOME, element: <Home /> },
-      { path: routePath.SOLVE, element: <Solve /> },
-    ],
-  },
+  { path: routePath.HOME, element: <Home /> },
+  { path: routePath.SOLVE, element: <Solve /> },
 ];
 
 export default pageRoutes;
