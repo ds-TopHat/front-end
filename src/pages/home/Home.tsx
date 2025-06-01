59c4a6db8d1d24a08a644d79277821aeb634ea1b
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-06-01 17:27:35 +0900

    setting: svgr 세팅 (#11)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,15 @@
 import { textStyle } from '@pages/home/home.css';
+import { themeVars } from '@styles/theme.css';
+
+import { IcMypage } from '@/shared/icons';
 
 const Home = () => {
   return (
     <>
-      <div className={textStyle} >Home 페이지</div>
+      <div className={textStyle}>Home 페이지</div>
       <div>
         2025 덕성여자대학교 컴퓨터공학과 졸업프로젝트 TopHat 팀 프론트엔드
+        <IcMypage width={30} height={30} color={`${themeVars.color.point}`} />
       </div>
     </>
   );
---
+++ src/shared/icons/index.ts
@@ -0,0 +1,1 @@
+export { default as IcMypage } from './IcMypage';
