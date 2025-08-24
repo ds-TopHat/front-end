bb135acce360064f1c9136c934ea358d91888d5f
hansoojeongsj <tnwjd3758@gmail.com>
2025-08-25 04:04:18 +0900

    feat: 노트 상세 (#37)

---
+++ src/pages/reviewNoteDetail/apis/queris.ts
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
+
+import { getReviewDetail } from './axios';
+import type { ReviewDetailResponseTypes } from '../types/api';
+
+import { QUERY_KEYS } from '@/shared/constants/queryKey';
+
+export const useGetReviewDetail = (questionId: number) => {
+  return useQuery<ReviewDetailResponseTypes, AxiosError>({
+    queryKey: [QUERY_KEYS.REVIEWNOTES, questionId],
+    queryFn: () => getReviewDetail(questionId),
+    enabled: !!questionId,
+  });
+};
